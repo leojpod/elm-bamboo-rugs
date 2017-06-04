692bb0d695c0c1b5ffc8700e2b71dd7bb551a020
Leo Jpod <leojpod@users.noreply.github.com>
2017-06-04 21:24:51 +0200

    adds a TypeScript Rug command handler to a Rug project

---
+++ .atomist/handlers/command/OpenPullRequest.ts
@@ -0,0 +1,30 @@
+import { CommandHandler, Intent, Parameter, Tags } from "@atomist/rug/operations/Decorators";
+import { CommandPlan, HandleCommand, HandlerContext, ResponseMessage } from "@atomist/rug/operations/Handlers";
+import { Pattern } from "@atomist/rug/operations/RugOperation";
+
+/**
+ * A create a branch and open a pull request for it.
+ */
+@CommandHandler("OpenPullRequest", "create a branch and open a pull request for it")
+@Tags("documentation")
+@Intent("create pr")
+export class OpenPullRequest implements HandleCommand {
+
+    @Parameter({
+        displayName: "Some Input",
+        description: "example of how to specify a parameter using decorators",
+        pattern: Pattern.any,
+        validInput: "a description of the valid input",
+        minLength: 1,
+        maxLength: 100,
+        required: false,
+    })
+    public inputParameter: string = "default value";
+
+    public handle(context: HandlerContext): CommandPlan {
+        const message = new ResponseMessage(`Successfully ran OpenPullRequest: ${this.inputParameter}`);
+        return CommandPlan.ofMessage(message);
+    }
+}
+
+export const openPullRequest = new OpenPullRequest();
