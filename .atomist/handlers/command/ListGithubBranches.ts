665554e3e355fdf5b244e3e0fd1a0721a141f719
Leo jPod <leo.jpod@gmail.com>
2017-06-05 15:18:05 +0200

    try to make a command to list github branches

--- .atomist/handlers/command/ListGithubBranches.ts
+++ .atomist/handlers/command/ListGithubBranches.ts
@@ -1,30 +1,117 @@
-import { CommandHandler, Intent, Parameter, Tags } from "@atomist/rug/operations/Decorators";
-import { CommandPlan, HandleCommand, HandlerContext, ResponseMessage } from "@atomist/rug/operations/Handlers";
-import { Pattern } from "@atomist/rug/operations/RugOperation";
+import {
+  CommandHandler,
+  Intent,
+  MappedParameter,
+  Parameter,
+  ParseJson,
+  ResponseHandler,
+  Tags
+} from '@atomist/rug/operations/Decorators'
+import {
+  CommandPlan,
+  HandleCommand,
+  HandleResponse,
+  HandlerContext,
+  MappedParameters,
+  MessageMimeTypes,
+  Response,
+  ResponseMessage
+} from '@atomist/rug/operations/Handlers'
+import {Pattern} from '@atomist/rug/operations/RugOperation'
+import * as mustache from 'mustache'
 
 /**
  * A return a list of existing branches of your repo.
  */
-@CommandHandler("ListGithubBranches", "return a list of existing branches of your repo")
-@Tags("documentation")
-@Intent("list branches")
+@CommandHandler('ListGithubBranches', 'return a list of existing branches of your repo')
+@Tags('documentation')
+@Intent('list branches')
 export class ListGithubBranches implements HandleCommand {
 
-    @Parameter({
-        displayName: "Some Input",
-        description: "example of how to specify a parameter using decorators",
-        pattern: Pattern.any,
-        validInput: "a description of the valid input",
-        minLength: 1,
-        maxLength: 100,
-        required: false,
+  @Parameter({
+    displayName: 'start with',
+    pattern: Pattern.any,
+    required: false
+  })
+  public startWith: string = ''
+
+  @MappedParameter(MappedParameters.GITHUB_REPOSITORY)
+  public repo: string
+
+  @MappedParameter(MappedParameters.GITHUB_REPO_OWNER)
+  public owner: string
+
+  @MappedParameter('atomist://correlation_id')
+  public corrid: string
+
+  public handle (context: HandlerContext): CommandPlan {
+    const plan = new CommandPlan()
+    // step 1 - send a request to github
+    plan.add({
+      instruction: {
+        kind: 'execute',
+        name: 'http',
+        parameters: {
+          url: `https://api.github.com/repos/${this.owner}/${this.repo}/branches`,
+          method: 'get',
+          config: {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `token #{github://user_token?scopes=repo}`
+            }
+          }
+        }
+      },
+      onSuccess: {
+        kind: 'respond',
+        name: 'SendReposBranches',
+        parameters: this
+      },
+      onError: new ResponseMessage("Buggar, it didn't work")
     })
-    public inputParameter: string = "default value";
-
-    public handle(context: HandlerContext): CommandPlan {
-        const message = new ResponseMessage(`Successfully ran ListGithubBranches: ${this.inputParameter}`);
-        return CommandPlan.ofMessage(message);
-    }
+    // step 2 - send a message once that's done
+    return plan
+  }
 }
+export const listGithubBranches = new ListGithubBranches()
 
-export const listGithubBranches = new ListGithubBranches();
+@ResponseHandler('SendReposBranches',
+    'Shows answers to a query on Stack Overflow')
+class ReposBranchesResponder implements HandleResponse<any> {
+
+  @Parameter({
+    displayName: 'start with',
+    pattern: Pattern.any,
+    required: false
+  })
+  public startWith: string = ''
+
+  @MappedParameter(MappedParameters.GITHUB_REPOSITORY)
+  public repo: string
+
+  @MappedParameter(MappedParameters.GITHUB_REPO_OWNER)
+  public owner: string
+
+  @MappedParameter('atomist://correlation_id')
+  public corrid: string
+
+  public handle ( @ParseJson response: Response<any>): CommandPlan {
+    return CommandPlan.ofMessage(
+        renderResults(response.body, this.startWith)
+    )
+  }
+}
+export let responder = new ReposBranchesResponder()
+
+function renderResults (result: any, query: string): ResponseMessage {
+
+  // if (result.items.length === 0) {
+  //   return new ResponseMessage('No results found.',
+  //       MessageMimeTypes.PLAIN_TEXT)
+  // }
+
+  // mark the last item for rendering purpose by mustache
+  // result.items[result.items.length - 1].last = true
+
+  return new ResponseMessage(JSON.stringify(result), MessageMimeTypes.PLAIN_TEXT)
+}
